57d7f0f04ca103d2f86bade8c03f3c5bfbd1025e
Ryan Maloney <malr@spooky.dog>
2024-02-27 17:35:38 +0000

    logic for the scroll added

--- desktoptrainboard.py
+++ desktoptrainboard.py
@@ -1,10 +1,20 @@
 import tkinter as tk
 from tkinter.font import Font
+import time
+from threading import Thread
 
 class MatrixText(tk.Label):
     """
     A derived class from the tk.Label class.
-    This class contains only stylistic
+    This class is here to provide stylistic
+    properties to the label, including the
+    font which shows as an LED matrix style
+    font.
+
+    --------------
+    custom methods
+    --------------
+        > scroll_text
     """
     def __init__(self, master):
         matrix_font = Font(
@@ -17,6 +27,27 @@ class MatrixText(tk.Label):
                         fg="#ffcc00",
                         justify="left",
                         anchor="nw")
+
+    def scroll_text(self):
+        """
+        Function that creates the illusion of
+        scrolling text by iterating through the
+        characters in the text and removing, replacing
+        the first and last characters.
+
+        !! This function is blocking
+        """
+        destin_text = self['text'] + " "
+        self.config(text = destin_text)
+        self.update()
+        self.update_idletasks()
+        while True:
+            for i in range(len(destin_text) + 1):
+                self.config(text = destin_text[i:] + destin_text[:i])
+                self.update()
+                self.update_idletasks()
+                time.sleep(0.5)
+        
 
 class DesktopTrainBoard(tk.Tk):
     '''
@@ -34,9 +65,12 @@ class DesktopTrainBoard(tk.Tk):
         self.attributes('-topmost', True)
         self.iconbitmap("assets/trainboard_icon.ico")
         #self.overrideredirect(True)
+    
+        self.ui_threads = []
 
         container = tk.Frame(self, bg="#000000")
         container.pack(expand=True, side="top", fill="both", anchor="w")
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_rowconfigure(1, weight=1)
         container.grid_rowconfigure(2, weight=1)
@@ -46,13 +80,14 @@ class DesktopTrainBoard(tk.Tk):
 
         message_container = tk.Frame(self, bg="#000000")
         message_container.pack(expand=True, side="top", fill="both", anchor="e")
+    
         message_container.grid_rowconfigure(0, weight=1)
         message_container.grid_columnconfigure(0, weight=1)
 
         dispatch_rows = [
             DispatchRow(container, 0, ("1st", "London Kings Cross", "11:00")),
-            DispatchRow(container, 1, ("2nd", "Exeter", "11:50")),
-            DispatchRow(container, 2, ("3rd", "Totnes", "12:00"))
+            DispatchRow(container, 1, ("2nd", "Exeter St Davids", "11:50")),
+            DispatchRow(container, 2, ("3rd", "Bradford Forster Square", "12:00"))
         ]
 
         container.update_idletasks()
@@ -63,16 +98,23 @@ class DesktopTrainBoard(tk.Tk):
             destination_actual_width = dispatch_row.final_destination.winfo_reqwidth()
             if (destination_actual_width > destination_alloted_width):
                 pass
-                #animate text
+                #TODO Scroll logic here -- threading?
+                
 
         MessageRow(message_container, "Some message...")
 
-        self.mainloop()
 
+class DispatchRow:
+    """
+    A class that represents each single
+    row of dispatch board, this includes
+    the:
+        - Ordinal position of the service (1st...3rd)
+        - Final destination of the service (i.e. Birmingham)
+        - Arrival or Departure time
 
-class DispatchRow(tk.Frame):
+    """
     def __init__(self, container: tk.Frame, row: int, dispatch_information: tuple):
-        super().__init__(master=container, bg="#000000")
 
         self.dispatch_row = row
 
@@ -88,6 +130,7 @@ class DispatchRow(tk.Frame):
         self.dispatch_ordinal.grid(row=row, column=0, sticky="nsew", padx=(0,0))
         self.final_destination.grid(row=row, column=1, sticky="nsew", padx=(0,10))
         self.dispatch_time.grid(row=row, column=2, sticky="nsew")
+    
 
 class MessageRow(tk.Frame):
     def __init__(self, master, message_to_display: str):
@@ -100,3 +143,7 @@ class MessageRow(tk.Frame):
 
 if __name__ == "__main__":
     d = DesktopTrainBoard()
+
+if __name__ == "__main__":
+    app = DesktopTrainBoard()
+    app.mainloop()
