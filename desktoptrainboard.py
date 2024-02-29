e31f3b6fdb315ae79e5997ce57ccbb1b960ad25c
Ryan Maloney <malr@spooky.dog>
2024-02-29 18:04:38 +0000

    moving closer to a solution to the threaded updates

--- desktoptrainboard.py
+++ desktoptrainboard.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from auxiliary_components import MatrixText
+from auxiliary_components import MatrixText, ThreadSafetyContainer
 import time
-from threading import Thread
+from threading import Thread, Event
 import requests
 
 class DesktopTrainBoard(tk.Tk):
@@ -17,7 +17,7 @@ class DesktopTrainBoard(tk.Tk):
         :attributes:
             dispatch_container: tk.Frame containing dispatch rows
             message_container: tk.Frame containing service message row
-            ui_threads: list of threads managing the updates to the gui
+            ui_threads: list of tuples for threads and events managing the updates to the gui
             dispatch_rows: list of DispatchRow objects within the dispatch_container 
         """
         self.title("Desktop Trainboard")
@@ -58,13 +58,16 @@ class DesktopTrainBoard(tk.Tk):
         self.dispatch_container.update_idletasks()
         self.dispatch_container.update()
 
-        self.message_container = tk.Frame(self, bg="#000000")
-        self.message_container.pack(expand=True, side="top", fill="both", anchor="e")
+        self.message_container = tk.Frame(self, bg="#ffffff")
+        self.message_container.pack(expand=True, side="top", fill="both", anchor="w")
     
         self.message_container.grid_rowconfigure(0, weight=1)
         self.message_container.grid_columnconfigure(0, weight=1)
 
         MessageRow(self.message_container, "Some message...")
+
+        self.message_container.update_idletasks()
+        self.message_container.update()
 
     def get_train_services(self, endpoint: str = None) -> list[dict]:
         """
@@ -80,10 +83,18 @@ class DesktopTrainBoard(tk.Tk):
         """
         train_services = requests.request(
             method="get",
-            url="put_in_variable"
+            url="http://trainboardapi.svc.spooknet.uk/api/get_station_departures"
         )
         train_services = train_services.json()
         return train_services["train_services"]
+
+    def safe_stop_ui_threads(self):
+        """
+        Safely stop any running UI threads by setting their
+        event and then waiting for the thread to join.
+        """
+        for thread_safety_container in self.ui_threads:
+            thread_safety_container.safe_stop_thread()
 
     def update_board(self):
         test_services = [
@@ -104,22 +115,29 @@ class DesktopTrainBoard(tk.Tk):
             }
         ]
 
-        for i, service in enumerate(test_services):
+        for i, service in enumerate(self.get_train_services()):
+            print("train services updated", service['destination'])
             self.dispatch_rows[i].set_row(service)
+
+        if self.ui_threads:
+            self.safe_stop_ui_threads()
+            self.ui_threads.clear()
 
         for dispatch_row in self.dispatch_rows:
             destination_alloted_width = self.dispatch_container.grid_bbox(column=1, row=dispatch_row.dispatch_row)[2]
             destination_actual_width = dispatch_row.final_destination.winfo_reqwidth()
             if (destination_actual_width > destination_alloted_width):
-                thread = Thread(target=dispatch_row.final_destination.scroll_text, daemon=True)
-                self.ui_threads.append(thread)
-                print(dispatch_row.final_destination['text'])
-                thread.start()
+                stop_event = Event()
+                thread = Thread(target=dispatch_row.final_destination.scroll_text, args=(stop_event,), daemon=True)
+                thread_safety_container = ThreadSafetyContainer(thread = thread, event = stop_event)
+                thread_safety_container.contained_thread.start()
+                self.ui_threads.append(thread_safety_container)
+
     
     def main_application(self):
         while True:
             self.update_board()
-            time.sleep(1)
+            time.sleep(60)
     
     def start_board(self):
         thread = Thread(target=self.main_application, daemon=True)
@@ -167,9 +185,9 @@ class DispatchRow:
 
 
 class MessageRow(tk.Frame):
-    def __init__(self, master, message_to_display: str):
-        super().__init__(master, background="#000000")
-        message = MatrixText(master)
+    def __init__(self, container: tk.Frame, message_to_display: str):
+        super().__init__(container, background="#000000")
+        message = MatrixText(container)
         message.config(text=message_to_display)
 
 if __name__ == "__main__":
